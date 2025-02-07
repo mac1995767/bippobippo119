4703b34c79a6966a4707c36daf43b424d37dbc03
kwanhyunkim <molba06@naver.com>
2025-02-07 15:13:00 +0900

    revise: PORT

--- server/src/index.js
+++ server/src/index.js
@@ -33,5 +33,5 @@ app.use('/api/hospitals/details/search', hospitalDetailSearchRoutes);
 app.use('/api/hospitals/subjects', hospitalSubjectRoutes); // 새로운 라우터 사용
 
 // 서버 실행
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
