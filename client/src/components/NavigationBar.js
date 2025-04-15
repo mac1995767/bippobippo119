3e7841a70fcead08cd31ba55d1d198d95812ee94
kwanhyunkim <55569899+KWANHYUNKIM@users.noreply.github.com>
2025-04-15 12:37:53 +0900

    [UPDATE] dropdown

--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -69,7 +69,7 @@ const NavigationBar = () => {
                 </button>
                 
                 {isDropdownOpen && (
-                  <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1">
+                  <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-[9999]">
                     <Link
                       to="/profile"
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
