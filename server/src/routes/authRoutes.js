63dfa14583e4214ea7eec93a0691284f30292043
mac1995767 <bippobippo119@gmail.com>
2025-05-19 15:31:11 +0900

    [UPDATE] cookie 수정

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -545,7 +545,7 @@ router.post('/kakao/callback', async (req, res) => {
         res.cookie('token', token, {
           httpOnly: true,
           secure: process.env.NODE_ENV === 'production',
-          sameSite: 'lax',
+          sameSite: 'none',
           maxAge: 24 * 60 * 60 * 1000
         });
 
